2c7952034b065972fb8bbc59135a0eaef701ae95
Wuij <TroyeWu@gmail.com>
2024-03-08 11:54:29 +0800

    add arraylist test

---
+++ DS/data_structures/src/test/java/algorithms/ArrayListTest.java
@@ -0,0 +1,41 @@
+package algorithms;
+import algorithms.Array_List.List;
+import algorithms.Array_List.ArrayList;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+
+
+
+
+public class ArrayListTest {
+
+    private final Logger logger = LoggerFactory.getLogger(ArrayListTest.class);
+
+    @Test
+    public void test_array_list() {
+        List<String> list = new ArrayList<>();
+        list.add("01");
+        list.add("02");
+        list.add("03");
+        list.add("04");
+        list.add("05");
+        list.add("06");
+        list.add("07");
+        list.add("08");
+        list.add("09");
+        list.add("10");
+        list.add("11");
+        list.add("12");
+
+        logger.info(list.toString());
+
+        list.remove(9);
+
+        logger.info(list.toString());
+    }
+
+}
