421322fd15c947f5afbef06722d366601913e655
Wuij <TroyeWu@gmail.com>
2024-03-20 13:30:40 +0800

    feat: add implementation of Trie and test

---
+++ DS/data_structures/src/test/java/algorithms/TrieTest.java
@@ -0,0 +1,28 @@
+package algorithms;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.alibaba.fastjson.JSON;
+import algorithms.Trie.Trie;
+
+public class TrieTest {
+
+    private final Logger logger = LoggerFactory.getLogger(TrieTest.class);
+
+    @Test
+    public void test_trie() {
+        Trie trie = new Trie();
+        // 存入
+        trie.insert("bat","大厂");
+        trie.insert("batch", "批量");
+        trie.insert("bitch", "彪子");
+        trie.insert("battle", "战斗");
+        logger.info(trie.toString());
+        // 检索
+        List<String> trieNodes = trie.searchPrefix("ba");
+        logger.info("测试结果：{}", JSON.toJSONString(trieNodes));
+    }
+
+}
